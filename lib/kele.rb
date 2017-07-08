8845d006d7c6b44b7b355a8cc0802cca4948f00e
Geoff Hull <geoffhull03@gmail.com>
2017-07-08 14:16:25 -0700

    Initialized and Authorized

---
+++ lib/kele.rb
@@ -0,0 +1,12 @@
+require 'httparty'
+
+class Kele
+  include HTTParty
+
+  def initialize(email, password)
+      response = self.class.post("https://www.bloc.io/api/v1/sessions", body: { "email": email, "password": password })
+
+      puts "Email/password combination not found. Please try again." if response.code != 200
+      @authorization_token = response["auth_token"]
+  end
+end
