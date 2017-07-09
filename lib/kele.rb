6b3c2b78b8c65bb5c53b86994f29af28dd4b7770
Geoff Hull <geoffhull03@gmail.com>
2017-07-09 15:19:17 -0700

    Able to retreive users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,23 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
 
   def initialize(email, password)
-      response = self.class.post("https://www.bloc.io/api/v1/sessions", body: { "email": email, "password": password })
+    @base_api_url = "https://www.bloc.io/api/v1"
+    response = self.class.post(@base_api_url + '/sessions', body: { "email": email, "password": password })
 
-      puts "Email/password combination not found. Please try again." if response.code != 200
-      @authorization_token = response["auth_token"]
+    puts "Email/password combination not found. Please try again." if response.code != 200
+    @authorization_token = response["auth_token"]
   end
+
+  def get_me
+    response = self.class.get(@base_api_url + '/users/me', headers: { "authorization" => @authorization_token })
+
+    puts "Email/password combination not found. Please try again." if response.code != 200
+    @user_data = JSON.parse(response.body)
+
+  end
+
 end
